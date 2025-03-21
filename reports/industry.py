1348e57ca18976f249983c5208bb81c9160e7263
Leo Chung <leo.chung97@gmail.com>
2025-03-21 14:20:03 -0400

    Add boilerplate code for industry report

---
+++ reports/industry.py
@@ -0,0 +1,37 @@
+from config import CLIENT, MODEL, TEMPERATURE, FORMATTED_DATE
+
+def industry_research(sector: str) -> str:
+    SYSTEM_INSTRUCTIONS = f"""
+    
+    """
+
+    USER_INSTRUCTIONS = f"""
+    
+    """
+
+    messages = [
+        {
+            "role": "system",
+            "content": SYSTEM_INSTRUCTIONS
+        },
+        {
+            "role": "user",
+            "content": USER_INSTRUCTIONS
+        }
+    ]
+
+    print(f"Creating an industry research report on {sector} using latest data available as of {FORMATTED_DATE}.")
+
+    response = CLIENT.chat.completions.create(
+        model=MODEL,
+        messages=messages,
+        temperature=TEMPERATURE
+    )
+    
+    output = []
+    formatted_citations = "\n".join(f"{number}. {source}" for number, source in enumerate(response.citations, start = 1))
+    output.extend((SYSTEM_INSTRUCTIONS, USER_INSTRUCTIONS, formatted_citations, response.choices[0].message.content))
+
+    print(f"Industry research report on {sector} created successfully.")
+
+    return output
